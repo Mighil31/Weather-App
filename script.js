85e09d412126a95e34b9043491ae16a2d389cb1c
mighil31 <mighildath@gmail.com>
2019-12-19 22:22:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const cityInput = document.querySelector("#form");
+const searchDiv = document.querySelector('#search');
+const main = document.querySelector('#main');
+const imageDiv = document.createElement('div');
+
+cityInput.value = "";
+let moved = false;
+
+cityInput.addEventListener("keyup", function(event){
+    if(event.keyCode === 13 )
+    {
+        console.log(cityInput.value);
+        if(!moved)
+        { 
+            moved = true;
+            searchDiv.classList.add("moveUp");
+            document.body.app
+        }
+    }
+});
