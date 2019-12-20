3f1b30c7e47256683db67cd79d082647ae6f3c04
mighil31 <mighildath@gmail.com>
2019-12-20 15:47:52 +0530

    Remove animation

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const cityInput = document.querySelector("#form");
 const searchDiv = document.querySelector('#search');
 const main = document.querySelector('#main');
 const imageDiv = document.createElement('div');
+const resultDiv = document.createElement('div');
 
 cityInput.value = "";
 let moved = false;
+let first = true;
 
 cityInput.addEventListener("keyup", function(event){
-    if(event.keyCode === 13 )
+    if(event.keyCode === 13)
     {
-        console.log(cityInput.value);
-        if(!moved)
-        { 
-            moved = true;
-            searchDiv.classList.add("moveUp");
-            setTimeout(function(){
-                main.appendChild(imageDiv);
-                imageDiv.classList.add("gif");
-            }, 1500);
-            
+        if(first) //first time entering then divs have to be loaded
+        {
+            first = false;
+            console.log(cityInput.value);
+            main.appendChild(imageDiv);
+            imageDiv.classList.add("gif");       
+            main.appendChild(resultDiv);
+            resultDiv.classList.add("result");
         }
     }
+
 });
